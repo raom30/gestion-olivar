0b7e84a1f9ae5416e048d8d91be8490d9a5d9d8a
Rafael Alvarez-Ossorio Martin <asir1almar@gmail.com>
2019-03-07 14:32:13 +0100

    Añadido archivo config.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
-
+const config = require('./config');
 const app = express();
+
 //Conexion a la base de datos
-mongoose.connect('mongodb://localhost:27017/gestion-olivar', { useNewUrlParser: true })
+mongoose.connect(config.db_uri, { useNewUrlParser: true })
   .then(db   => console.log ('Conexión correcta a la BD'))
   .catch(err => console.log ('Error en la conexión a la BD'));
 
